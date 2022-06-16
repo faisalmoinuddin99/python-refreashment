e50373db4420be8c327fdc2cf1d2188901592797
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-06-16 12:21:07 +0530

    Escape Sequences

--- tut6.py
+++ tut6.py
@@ -6,3 +6,16 @@ Multi line comments
 
 print("Hello world","My name is faisal", end=",")
 print("I live in mumbai")
+
+
+# Escape Seqence in python
+
+"""
+Python escape sequence ignore
+
+To ignore all the escape sequences in the string, we have to make a 
+string as a raw string using ‘r’ before the string. 
+After that escape sequence will also be consider as normal characters. 
+Let's take an example.
+"""
+print(r"Hello\nWorld")
