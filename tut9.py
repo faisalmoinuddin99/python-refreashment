b2a16b01598668be817378f7786292f9c45e9f32
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-06-16 15:48:05 +0530

    Tuples in Python

--- tut9.py
+++ tut9.py
@@ -22,4 +22,33 @@ numbers.insert(3,90)
 print(numbers) # [2, 3, 7, 90, 9, 11, 10]
 numbers.remove(90)
 print(numbers) #[2, 3, 7, 9, 11, 10]
-print(numbers.pop())
+print(numbers.pop())  
+
+
+# Tuple in Python
+
+"""
+A] Mutuable === List (Can change)
+B] Immutuable === Tuple (Cannot change)
+"""
+
+tp = (1,2,4)
+print(type(tp)) # <class 'tuple'>
+print(tp)
+
+
+# Swap two number
+
+a = 10
+b = 20
+
+temp = a
+a = b 
+b = temp
+print(a,b) # 20 10
+
+x = 100
+y = 200
+
+x,y = y,x
+print(x,y) # 200 100
