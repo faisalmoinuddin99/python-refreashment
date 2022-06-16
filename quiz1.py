a978b1186e7527590edbd579c99655d95b479b4b
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-06-16 12:55:01 +0530

    Quiz: Basic Calculator with addition feature

---
+++ quiz1.py
@@ -0,0 +1,7 @@
+# Caluculator with only addition functionality
+
+number1 = int(input("Enter your first number:"))
+number2 = int(input("Enter your second number:"))
+
+addition = number1 + number2
+print("Result:", addition)
