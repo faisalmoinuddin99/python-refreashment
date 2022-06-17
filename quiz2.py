5ccefaced47132749411e14885d7f45a09a2ff9d
MohdFaisal Suleman <MohdFaisal.Suleman@lntinfotech.com>
2022-06-17 11:12:59 +0530

    Done with Quiz 2

---
+++ quiz2.py
@@ -0,0 +1,17 @@
+# Create a dictionary and take input from the user and return the meaning of the word from the dictionary
+
+oxfordDictionary =  {
+    "mutuable":"Can be change",
+    "immutuable":"Cannot change",
+    "buzzkill":"Person or thing that has a depressing effect",
+    "chillax":"To calm down and relax"
+}
+
+searchWord = input("Enter a word:")
+print(oxfordDictionary[searchWord])
+
+# output
+"""
+Enter a word:buzzkill 
+Person or thing that has a depressing effect
+"""
