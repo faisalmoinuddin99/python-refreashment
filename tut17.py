bf74864f7f2cfca3780917336b55adccc415d85d
MohdFaisal Suleman <MohdFaisal.Suleman@lntinfotech.com>
2022-06-18 09:07:39 +0530

    if else short hand version:

---
+++ tut17.py
@@ -0,0 +1,8 @@
+# Shorthand if else
+
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+
+# if a > b: print("A is greater than B")
+
+print("A is greater than B") if a > b else print("A is less than B")
