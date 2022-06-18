9ea7d1656e731db4f0157d4586fde6ba3bdca99b
MohdFaisal Suleman <MohdFaisal.Suleman@lntinfotech.com>
2022-06-18 07:52:30 +0530

    While loop in Python

---
+++ tut14.py
@@ -0,0 +1,20 @@
+# while loops
+
+i = 0
+
+while i < 10:
+    # print(i)
+    i += 1
+
+
+
+
+todoList = ["prayer","gym","brush","breakfast"]
+
+i = 0
+while i in range(len(todoList)):
+    print(todoList[i])
+    i += 1
+
+for todo in todoList:
+    print(todo)
