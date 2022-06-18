184f1a77e781f4bce384b2fc213faa94c4dc2f3f
MohdFaisal Suleman <MohdFaisal.Suleman@lntinfotech.com>
2022-06-18 15:50:13 +0530

    Scope, Global Vaiables

---
+++ tut24.py
@@ -0,0 +1,32 @@
+# Scope, Global variables
+
+
+l = 20 #global scope
+m = 50
+
+def func1():
+    l = 10 #Local variables
+    global m # global keyword is used to give permission to access global varibales to do any updation
+    m = m + 20
+    print(l, m)
+
+func1()
+print(l)
+
+
+
+def faisal():
+    x = 20
+    def rohan():
+        global x
+        x = 88
+    print("Before calling rohan()", x)
+    rohan()
+    print("after calling rohan()",x)
+
+faisal()
+"""
+Before calling rohan() 20
+after calling rohan() 20
+"""
+print(x) # 88
