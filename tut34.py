564b5b5943a283fb96753e541b6e2c33ff30472b
MohdFaisal Suleman <MohdFaisal.Suleman@lntinfotech.com>
2022-06-19 19:03:05 +0530

    Join Function | Python

---
+++ tut34.py
@@ -0,0 +1,10 @@
+# join function | python
+
+lst = ["Jhon cena","Randy","Sheamus","khali","jinder mahal"]
+
+# for item in lst:
+#     print(item, "and", end=" ")
+a = ", ".join(lst)
+
+
+print(a,"other wwe superstar") # Jhon cena, Randy, Sheamus, khali, jinder mahal other wwe superstar
